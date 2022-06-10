a18253d62af77006cc2d33f335240d802fe96c7f
Sultan <sadli0602028@gmail.com>
2022-06-10 10:41:03 +0600

    Added new feature

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-function findLongestWordLength(str) {
-    let words = str.split(' ');
-    let maxLength = 0;
-
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
-    }
-  }
-    console.log (maxLength);
-  }
-  findLongestWordLength("The quick brown fox jumped over the lazy dog");
-  
-=======
-console.log('connected?')
->>>>>>> 70bd001f24ecc0162cef434d9bd275db70358930
+console.log('connected?') 
